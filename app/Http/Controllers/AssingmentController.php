2bfd9594a8773eb814f3e24a44a5462b16274dc8
farukwebcode21 <farukahmadmk@yahoo.com>
2023-05-22 11:17:52 +0600

    start laravel assingment

--- app/Http/Controllers/AssingmentController.php
+++ app/Http/Controllers/AssingmentController.php
@@ -6,7 +6,35 @@ use Illuminate\Http\Request;
 
 class AssingmentController extends Controller
 {
+
+    // 
     function postUserName(Request $request){
         $name = $request->input('name');
+        // return $name;
+    }
+    // 
+    function postUserAgent(Request $request){
+        $userAgent = $request->header('User-Agent');
+        return $userAgent;
+
+    }
+    // 
+    function getStore(Request $request){
+        $page = $request->query('page');
+        if($page === null){
+            $page = null;
+        }
+    }
+
+    // 
+
+    function createJsonData(){
+        $data =[
+            "message" =>"Success",
+            "data"=>[
+                "name" =>"John Doe",
+                "age"=>25
+                ]
+            ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/submit',[AssingmentController::class, 'postUserName']); 
+Route::post('/submit/{name}',[AssingmentController::class, 'postUserName']); 
